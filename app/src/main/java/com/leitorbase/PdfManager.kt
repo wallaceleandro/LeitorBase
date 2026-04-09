c27f73a8b0c8363ba1c53d5d9deeafe4cf0ff1e9
Wallace Leandro <wallaceleandro8@gmail.com>
2026-04-09 12:38:37 -0300

    Leitura real de PDF implementada

--- app/src/main/java/com/leitorbase/PdfManager.kt
+++ app/src/main/java/com/leitorbase/PdfManager.kt
@@ -2,15 +2,41 @@ package com.leitorbase
 
 import android.content.Context
 import android.net.Uri
+import android.util.Log
+import com.itextpdf.text.pdf.PdfReader
+import com.itextpdf.text.pdf.parser.PdfTextExtractor
 
 object PdfManager {
 
     fun pdfSelecionado(context: Context, uri: Uri?): String {
 
-        return if (uri != null) {
-            "PDF selecionado com sucesso"
-        } else {
-            "Erro ao selecionar PDF"
+        if (uri == null) return "Erro ao abrir PDF"
+
+        return try {
+
+            val inputStream = context.contentResolver.openInputStream(uri)
+            val reader = PdfReader(inputStream)
+
+            val stringBuilder = StringBuilder()
+
+            for (i in 1..reader.numberOfPages) {
+                val texto = PdfTextExtractor.getTextFromPage(reader, i)
+                stringBuilder.append(texto)
+            }
+
+            reader.close()
+
+            val resultado = stringBuilder.toString()
+
+            if (resultado.isBlank()) {
+                "PDF sem texto ou não suportado"
+            } else {
+                resultado
+            }
+
+        } catch (e: Exception) {
+            Log.e("PDF_ERRO", e.toString())
+            "Erro ao ler PDF"
         }
     }
 }
